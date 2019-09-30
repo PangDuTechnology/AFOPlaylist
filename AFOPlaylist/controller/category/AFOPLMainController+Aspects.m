fad1d3d09e608a81412c21f9f1140f61d593b4e7
PangDu <xian312117@gmail.com>
2019-09-30 20:08:28 +0800

    commit

--- AFOPlaylist/controller/category/AFOPLMainController+Aspects.m
+++ AFOPlaylist/controller/category/AFOPLMainController+Aspects.m
@@ -22,16 +22,38 @@
                                                                                                                                                                      @"title" : name,
                                                                                                                                                                      @"direction":@(screen)
                                                                                                                                                                      }];
-        NSString *newString =[[AFORouterManager shareInstance] settingRoutesParameters:@{
-                                                                                         @"modelName" :   JLRouteFunctionPlaylist,                                        @"controller" : @"AFOMediaPlayController",
-                                                                                         @"present" : NSStringFromClass([self class]),
-                                                                                         @"action" :@"push",
-                                                                                         @"value" : path,
-                                                                                         @"title" : name,
-                                                                                         @"direction" : @(screen)
-                                                                                         }];
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:baseStr]];
+//        NSString *newString =[[AFORouterManager shareInstance] settingRoutesParameters:];
+        NSDictionary *dictionary = @{
+                                     @"modelName" :   JLRouteFunctionPlaylist,                                        @"controller" : @"AFOMediaPlayController",
+                                     @"present" : NSStringFromClass([self class]),
+                                     @"action" :@"push",
+                                     @"value" : path,
+                                     @"title" : name,
+                                     @"direction" : @(screen)
+                                     };
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[self convertToJsonData:dictionary]]];
     } error:NULL];
+}
+- (NSString *)convertToJsonData:(NSDictionary *)dict{
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString;
+    
+    if (!jsonData) {
+        NSLog(@"%@",error);
+    }else{
+        jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+    
+    NSMutableString *mutStr = [NSMutableString stringWithString:jsonString];
+    //    NSRange range = {0,jsonString.length};
+    //    //去掉字符串中的空格
+    //    [mutStr replaceOccurrencesOfString:@" " withString:@"" options:NSLiteralSearch range:range];
+    NSRange range2 = {0,mutStr.length};
+    //去掉字符串中的换行符
+    [mutStr replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:range2];
+    
+    return mutStr;
 }
 - (void)dealloc{
     NSLog(@"AFOPLMainController+Aspects dealloc");
